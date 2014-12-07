eb52992efa4e62d08ea710fc0ab2b5d480a29aa3
eshi17 <eshi17@punahou.edu>
2014-12-07 01:20:00 -1000

    Commented code
    Comments on the side

--- Maze_Game/MazeGame/MazeGame.ino
+++ Maze_Game/MazeGame/MazeGame.ino
@@ -6,20 +6,20 @@
 
 // Global Variables
 int xcoord = 0;
-int ycoord = 7;
+int ycoord = 7; //original location of player
 int xsharka = 2;
 int ysharka = 2;
 int xsharkb = 7;
 int ysharkb = 7;
 int xsharkc = 1;
-int ysharkc = 0;
+int ysharkc = 0; //original locations of sharks
 boolean aTreasure = false;
 boolean bTreasure = false;
 boolean cTreasure = false;
-boolean dTreasure = false;
+boolean dTreasure = false;//existance of treasure
 boolean aAShark = false;
 boolean bBShark = false;
-boolean cCShark = false;
+boolean cCShark = false;//direction of shark
 
 void setup()                    // run once, when the sketch starts
 {
@@ -33,14 +33,17 @@ void loop()                     // run over and over again
   Serial.println(xcoord); //checks x print
   Serial.print("y = ");
   Serial.println(ycoord); //checks y print
-  EditColor(Orange, 10, 0, 2);
+  EditColor(Orange, 10, 0, 2); //edited orange to a vibrant pink color
+  EditColor(Blue, 0, 1, 2);//edited blue to a duller blue
+  EditColor(White, 16, 6, 7);//edited white to a duller white
+  
 
   CheckButtonsDown();
   if (Button_Right) //All code that prevents you from walking through walls comes from Brent Uramoto
   {
     if(xcoord < 7)
     {
-      if(ReadPx(xcoord+1, ycoord) != White)
+      if(ReadPx(xcoord+1, ycoord) != White) //if the dot is not on a white pixel, it can move
       {
         xcoord++;
       }
@@ -85,17 +88,16 @@ void loop()                     // run over and over again
     DrawPx(xcoord,ycoord,Yellow);
   }
     DisplaySlate();
-    delay(400);
+    delay(400); //had trouble making it "real time". Compromised both sharks and player so that it moves at an equal speed, although that makes the player a little slow
     ClearSlate();
-  Ocean();
-  drawPlatforms();
-  Treasure();
-  winGame();
+  Ocean(); //blue screen
+  drawPlatforms(); //white walls
+  Treasure();//pink dots
+  winGame();//yellow star+sounds
 }
   
 void drawPlatforms()
 {
-    EditColor(White, 16, 6, 7);
     DrawPx(0, 4, White);
     DrawPx(1, 4, White);
     DrawPx(2, 4, White);
@@ -116,37 +118,36 @@ void drawPlatforms()
     DrawPx(6, 6, White);
     DrawPx(1, 7, White);
     DrawPx(1, 6, White);
-    DrawPx(5, 4, White);
+    DrawPx(5, 4, White); //no other way to code than hard code
 }
 
 void Ocean()
 {
-  EditColor(Blue, 0, 1, 2);
      for (int i = 0; i < 8; i++)
      for (int a = 0; a < 8; a++)
-      DrawPx(i, a, Blue);
+      DrawPx(i, a, Blue); //makes whole screen blue
 }
 
 void Treasure()
 {
   if(xcoord == 0 && ycoord == 3)
   {
-    aTreasure = true;
+    aTreasure = true; //if player is on 0,3 then aTreasure variable becomes true (from false)
   }
   
-  if (aTreasure == false)
+  if (aTreasure == false) //originally aTreasure is false, so it appears pink
   {
     DrawPx(0,3,Orange);
   }
 
-  if (aTreasure == true)
+  if (aTreasure == true)//when turned true, treasure becomes blue, the same color as the ocean
   {
     DrawPx(0,3,Blue);
   }
   
   if(xcoord == 6 && ycoord == 4)
   {
-    bTreasure = true;
+    bTreasure = true; //same concept as above
   }
   
   if (bTreasure == false)
@@ -192,7 +193,7 @@ void Treasure()
 
 void aShark()
 {
-  DrawPx(xsharka, ysharka, Dark);
+  DrawPx(xsharka, ysharka, Dark);//if dot is on location, then aAShark is false
   if (xsharka == 2)
   {
     if (ysharka == 2)
@@ -200,7 +201,7 @@ void aShark()
       aAShark = false;
     }
   }
-  if (aAShark == false)
+  if (aAShark == false)//if location of shark is false, then shark moves towards designated location
   {
     xsharka = xsharka + 1;
   }
@@ -208,15 +209,15 @@ void aShark()
   {
     if (ysharka == 2)
     {
-      aAShark = true;
+      aAShark = true;//once the shark reaches the rebound location (6,2), then aAShark becomes true
     }
   }
-  if (aAShark == true)
+  if (aAShark == true) // if aAShark is true, then it goes towards original location
   {
     xsharka = xsharka - 1;
   }
   
-  DrawPx(xsharkb, ysharkb, Dark);
+  DrawPx(xsharkb, ysharkb, Dark); //same concept as above
   if (xsharkb == 7)
   {
     if (ysharkb == 7)
@@ -263,7 +264,7 @@ void aShark()
   {
     xsharkc = xsharkc - 1;
   }
-  if(ReadPx(xcoord, ycoord) == Dark)
+  if(ReadPx(xcoord, ycoord) == Dark) //the game over; if player touches sharks, it goes back to original location; eaten treasures remain eaten (gives room for strategy).
    {
      (xcoord = 0);
      (ycoord = 7);
@@ -271,7 +272,6 @@ void aShark()
      drawPlatforms();
      Treasure();
      Tone_Start(ToneC3, 50);
-     Tone_Start(ToneF3, 50);  
    }
 }
 
@@ -283,7 +283,7 @@ void winGame()
       {
         if (cTreasure == true)
         {
-          if (dTreasure == true)
+          if (dTreasure == true) //if all treasures are true (all treasures have been eaten), then game is won
   {
   Ocean();
   DrawPx(4,7,DimYellow);
@@ -316,19 +316,19 @@ void winGame()
   DrawPx(5,1,DimYellow);
   DrawPx(6,1,DimYellow);
   DrawPx(1,0,DimYellow);
-  DrawPx(6,0,DimYellow);
+  DrawPx(6,0,DimYellow); //giant yellow star
     Tone_Start(ToneA5,100);
       delay(150);
     Tone_Start(ToneB5,100);
       delay(150);
     Tone_Start(ToneC5,100);
-      delay(150);
+      delay(150); //decent sounding noise
   aTreasure = false;
   bTreasure = false;
   cTreasure = false;
-  dTreasure = false;
+  dTreasure = false; //treasures become false, and game goes back to original state
   xcoord = 0;
-  ycoord = 7;
+  ycoord = 7; //player returns to original location
   }
         }
       }
