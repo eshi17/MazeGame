62b818a82f945cbbd9b1d71fd1f34dbffeb87008
eshi17 <eshi17@punahou.edu>
2014-12-07 00:45:09 -1000

    Final Commit!(?)
    Fixed bug where treasure doesn’t go away on point 0,3. Fixed bug where
    player and sharks alternate blinking, but the player reaction time is a
    little slow. Don’t know if I can fix. Also included a game over with
    sounds! :D

--- Maze_Game/MazeGame/MazeGame.ino
+++ Maze_Game/MazeGame/MazeGame.ino
@@ -1,6 +1,6 @@
 /*
-  Moving_Dot.pde
- */
+  MazeGame.pde
+*/
 
 #include <MeggyJrSimple.h>    // RequiYellow code, line 1 of 2.
 
@@ -23,52 +23,52 @@ boolean cCShark = false;
 
 void setup()                    // run once, when the sketch starts
 {
-  MeggyJrSimpleSetup();      // RequiYellow code, line 2 of 2.
+  MeggyJrSimpleSetup();
   Serial.begin(9600);
 }
 
 void loop()                     // run over and over again
 {
   Serial.print("x = ");
-  Serial.println(xcoord);
+  Serial.println(xcoord); //checks x print
   Serial.print("y = ");
-  Serial.println(ycoord);
+  Serial.println(ycoord); //checks y print
   EditColor(Orange, 10, 0, 2);
-  
-  CheckButtonsPress();
+
+  CheckButtonsDown();
   if (Button_Right) //All code that prevents you from walking through walls comes from Brent Uramoto
   {
     if(xcoord < 7)
+    {
+      if(ReadPx(xcoord+1, ycoord) != White)
       {
-        if(ReadPx(xcoord+1, ycoord) != White)
-        {
-          xcoord++;
-        }
+        xcoord++;
       }
+    }
   }
   
   if (Button_Left)
   {
     if(xcoord > 0)
+    {
+      if(ReadPx(xcoord-1, ycoord) != White)
       {
-        if(ReadPx(xcoord-1, ycoord) != White)
-        {
-          xcoord--;
-        }
+        xcoord--;
       }
+    }
   }
   
   if (Button_Up)
   {
-    if(ycoord < 7)
+  if(ycoord < 7)
+    {
+      if(ReadPx(xcoord, ycoord+1) != White)
       {
-        if(ReadPx(xcoord, ycoord+1) != White)
-          {
-            ycoord++;
-          }
+        ycoord++;
       }
+    }
   }
-        
+  
   if (Button_Down)
   {
     if(ycoord > 0)
@@ -80,20 +80,19 @@ void loop()                     // run over and over again
       }
   }
   
-  aShark();
-  DisplaySlate();
-  delay(1000);
-  
-  DrawPx(xcoord,ycoord,Yellow);
-  DisplaySlate();
-  delay(1000);
-  ClearSlate();
-  
+  {
+    aShark();
+    DrawPx(xcoord,ycoord,Yellow);
+  }
+    DisplaySlate();
+    delay(400);
+    ClearSlate();
   Ocean();
   drawPlatforms();
   Treasure();
+  winGame();
 }
-
+  
 void drawPlatforms()
 {
     EditColor(White, 16, 6, 7);
@@ -130,47 +129,62 @@ void Ocean()
 
 void Treasure()
 {
+  if(xcoord == 0 && ycoord == 3)
+  {
+    aTreasure = true;
+  }
   
-if (aTreasure == false)
+  if (aTreasure == false)
   {
     DrawPx(0,3,Orange);
   }
+
+  if (aTreasure == true)
+  {
+    DrawPx(0,3,Blue);
+  }
   
-if(xcoord == 6 && ycoord == 4)
-  bTreasure = true;
+  if(xcoord == 6 && ycoord == 4)
+  {
+    bTreasure = true;
+  }
   
-if (bTreasure == false)
+  if (bTreasure == false)
   {
     DrawPx(6,4,Orange);
   }
 
-if (bTreasure == true)
+  if (bTreasure == true)
   {
     DrawPx(6,4,Blue);
   }
   
-if(xcoord == 7 && ycoord == 1)
+  if(xcoord == 7 && ycoord == 1)
+  {
   cTreasure = true;
+  }
   
-if (cTreasure == false)
+  if (cTreasure == false)
   {
     DrawPx(7,1,Orange);
   }
 
-if (cTreasure == true)
+  if (cTreasure == true)
   {
     DrawPx(7,1,Blue);
   }
 
-if(xcoord == 2 && ycoord == 5)
+  if(xcoord == 2 && ycoord == 5)
+  {
   dTreasure = true;
+  }
   
-if (dTreasure == false)
+  if (dTreasure == false)
   {
     DrawPx(2,5,Orange);
   }
 
-if (dTreasure == true)
+  if (dTreasure == true)
   {
     DrawPx(2,5,Blue);
   }
@@ -186,10 +200,10 @@ void aShark()
       aAShark = false;
     }
   }
-    if (aAShark == false)
-    {
-      xsharka = xsharka + 1;
-    }
+  if (aAShark == false)
+  {
+    xsharka = xsharka + 1;
+  }
   if (xsharka == 6)
   {
     if (ysharka == 2)
@@ -197,10 +211,10 @@ void aShark()
       aAShark = true;
     }
   }
-    if (aAShark == true)
-    {
-      xsharka = xsharka - 1;
-    }
+  if (aAShark == true)
+  {
+    xsharka = xsharka - 1;
+  }
   
   DrawPx(xsharkb, ysharkb, Dark);
   if (xsharkb == 7)
@@ -210,10 +224,10 @@ void aShark()
       bBShark = false;
     }
   }
-    if (bBShark == false)
-    {
-      ysharkb = ysharkb - 1;
-    }
+  if (bBShark == false)
+  {
+    ysharkb = ysharkb - 1;
+  }
   if (xsharkb == 7)
   {
     if (ysharkb == 2)
@@ -221,10 +235,10 @@ void aShark()
       bBShark = true;
     }
   }
-    if (bBShark == true)
-    {
-      ysharkb = ysharkb + 1;
-    }
+  if (bBShark == true)
+  {
+    ysharkb = ysharkb + 1;
+  }
     
   DrawPx(xsharkc, ysharkc, Dark);
   if (xsharkc == 2)
@@ -234,10 +248,10 @@ void aShark()
       cCShark = false;
     }
   }
-    if (cCShark == false)
-    {
-      xsharkc = xsharkc + 1;
-    }
+  if (cCShark == false)
+  {
+    xsharkc = xsharkc + 1;
+  }
   if (xsharkc == 8)
   {
     if (ysharkc == 0)
@@ -245,18 +259,78 @@ void aShark()
       cCShark = true;
     }
   }
-    if (cCShark == true)
-    {
-      xsharkc = xsharkc - 1;
-    }
+  if (cCShark == true)
+  {
+    xsharkc = xsharkc - 1;
+  }
   if(ReadPx(xcoord, ycoord) == Dark)
    {
      (xcoord = 0);
      (ycoord = 7);
-     DrawPx(0, 3, Orange);
-     DrawPx(2, 5, Orange);
-     DrawPx(6, 4, Orange);
-     DrawPx(7, 1, Orange);
-     Tone_Start(ToneC3, 50);  
+     Ocean();
+     drawPlatforms();
+     Treasure();
+     Tone_Start(ToneC3, 50);
+     Tone_Start(ToneF3, 50);  
    }
+}
+
+void winGame()
+{
+  if (aTreasure == true)
+    {
+      if (bTreasure == true)
+      {
+        if (cTreasure == true)
+        {
+          if (dTreasure == true)
+  {
+  Ocean();
+  DrawPx(4,7,DimYellow);
+  DrawPx(3,6,DimYellow);
+  DrawPx(4,6,DimYellow);
+  DrawPx(2,5,DimYellow);
+  DrawPx(3,5,DimYellow);
+  DrawPx(4,5,DimYellow);
+  DrawPx(5,5,DimYellow);
+  DrawPx(0,4,DimYellow);
+  DrawPx(1,4,DimYellow);
+  DrawPx(2,4,DimYellow);
+  DrawPx(3,4,DimYellow);
+  DrawPx(4,4,DimYellow);
+  DrawPx(5,4,DimYellow);
+  DrawPx(6,4,DimYellow);
+  DrawPx(7,4,DimYellow);
+  DrawPx(1,3,DimYellow);
+  DrawPx(2,3,DimYellow);
+  DrawPx(3,3,DimYellow);
+  DrawPx(4,3,DimYellow);
+  DrawPx(5,3,DimYellow);
+  DrawPx(6,3,DimYellow);
+  DrawPx(2,2,DimYellow);
+  DrawPx(3,2,DimYellow);
+  DrawPx(4,2,DimYellow);
+  DrawPx(5,2,DimYellow);
+  DrawPx(1,1,DimYellow);
+  DrawPx(2,1,DimYellow);
+  DrawPx(5,1,DimYellow);
+  DrawPx(6,1,DimYellow);
+  DrawPx(1,0,DimYellow);
+  DrawPx(6,0,DimYellow);
+    Tone_Start(ToneA5,100);
+      delay(150);
+    Tone_Start(ToneB5,100);
+      delay(150);
+    Tone_Start(ToneC5,100);
+      delay(150);
+  aTreasure = false;
+  bTreasure = false;
+  cTreasure = false;
+  dTreasure = false;
+  xcoord = 0;
+  ycoord = 7;
+  }
+        }
+      }
+    }
 }
